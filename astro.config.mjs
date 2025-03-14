4df8021273a9c5eee35092672ba6dffd2a34313f
Nils Ingwersen <ingwersen@ubilabs.com>
2025-03-14 15:52:10 +0100

    feat: added trailing slash never

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: "never",
   site: "https://ingwersen.dev",
   integrations: [mdx(), sitemap()],
 });
