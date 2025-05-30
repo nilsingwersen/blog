ca857c86072db1f84ec2b89add86939044398251
Nils Ingwersen <ingwersen@ubilabs.com>
2025-05-30 14:30:45 +0200

    feat: added cloudflare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "never",
+
   build: {
     format: "file",
   },
+
   site: "https://ingwersen.dev",
   integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
 });
