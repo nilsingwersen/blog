dac3f49126ae2dfb0dbd72493c7ef7c613d3ee13
Nils Ingwersen <ingwersen@ubilabs.com>
2026-04-09 11:22:47 +0200

    feat: updated to astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,40 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: "never",
-
-  build: {
-    format: "file",
-  },
-
-  site: "https://ingwersen.dev",
+  site: 'https://ingwersen.dev',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
 
-    imageService: "cloudflare",
-  }),
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Atkinson',
+      cssVariable: '--font-atkinson',
+      fallbacks: ['sans-serif'],
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/atkinson-regular.woff'],
+            weight: 400,
+            style: 'normal',
+            display: 'swap',
+          },
+          {
+            src: ['./src/assets/fonts/atkinson-bold.woff'],
+            weight: 700,
+            style: 'normal',
+            display: 'swap',
+          },
+        ],
+      },
+    },
+  ],
+
+  adapter: cloudflare(),
 });
